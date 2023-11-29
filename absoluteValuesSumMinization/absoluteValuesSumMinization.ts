68a0c1ab9b9af4800bf9d272550acd27931466a8
Allan Rolle <allan.rolle@laposte.net>
2023-11-29 16:23:28 +0100

    algo

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -1,8 +1,9 @@
-export function absoluteValuesSumMinimization(a: number[]): number {
-    return 5;
+export function absoluteValuesSumMinimization(array: number[]): number {
+  const isEven = array.length % 2  === 0;
+  return isEven ? array[array.length / 2 - 1] : array[Math.floor(array.length / 2)];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));          // 4
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));       // 4 
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));    // 7
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8])); // 7
--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,6 @@
 export function addBorder(picture: string[]): string[] {
-
+    const borderLength = picture[0].length + 2;
+    // to do....
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
